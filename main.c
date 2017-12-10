10d5dbcf1c689961ed4344c5d52cabd00bfa2bdb
AndrewChe <Chernjakov2@gmail.com>
2017-12-10 21:49:39 +0300

    bn_add

--- main.c
+++ main.c
@@ -5,10 +5,12 @@ int main() {
 	bn* big_number = bn_new();
 	bn* big_number2 = bn_init(big_number);
 	
-	bn_init_int(big_number, 10);
+	bn_init_int(big_number, 10000000);
 	bn_init_int(big_number2, 20);
 	
+	bn* sum = bn_add (big_number, big_number2);
 
+	bn_delete(sum);
 	bn_delete(big_number);
 	bn_delete(big_number2);
 	
